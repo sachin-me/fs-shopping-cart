71823fc8dca67f27695170d0dc34cb2e70cd5666
sachin-me <sachinkumartiwari363@gmail.com>
2021-09-16 23:31:33 +0530

    Added Loader

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -1,54 +1,56 @@
-import React, { Component } from 'react';
-import { connect } from 'react-redux';
+import React, { Component } from "react";
+import { connect } from "react-redux";
 import { Link } from "react-router-dom";
-import actions from './../store/actions';
-import ProductLists from './ProductLists';
-import Search from './Search';
-import Category from './Category';
+import actions from "./../store/actions";
+import ProductLists from "./ProductLists";
+import Search from "./Search";
+import Category from "./Category";
+import RLoader from "./RLoader";
 // import RLoader from './RLoader';
 
 class Home extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      loading: true
-    }
+      loading: true,
+    };
   }
 
   componentDidMount() {
-    this.props.dispatch(actions.getProducts(success => {
-      if (success) {
-        this.setState({
-          loading: false
+    this.props.dispatch(
+      actions
+        .getProducts((success) => {
+          if (success) {
+            this.setState({
+              loading: false,
+            });
+          } else {
+            this.setState({
+              loading: false,
+            });
+          }
         })
-      } else {
-        this.setState({
-          loading: false
-        })
-      }
-    }).bind(this));
+        .bind(this)
+    );
   }
-  
+
   render() {
     const { loading } = this.state;
+    if (loading) {
+      return <RLoader />;
+    }
     return (
       <div className="main-wrapper">
-        {
-          !loading ? (
-            <>
-              <div className="search-bar">
-                <Search />
-                <Category />
-              </div>
-              <div>
-                <ProductLists />
-              </div>
-            </>
-          ) : 'loading...'
-        }
+        <div className="search-bar">
+          <Search />
+          <Category />
+        </div>
+        <div>
+          <ProductLists />
+        </div>
       </div>
     );
   }
 }
 
-export default connect(null)(Home)
+export default connect(null)(Home);
